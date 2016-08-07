95408225e4cde9523fb8a16c5e1ac5db3cdc3161
Scott Stamp <scott@hypermine.com>
2016-08-07 07:25:58 -0230

    tidied up accept-tos.py

--- scripts/accept-tos.py
+++ scripts/accept-tos.py
@@ -11,19 +11,15 @@ import pprint
 import time
 import threading
 
-def worker(num):
+def accept_tos(username, password):
 	api = PGoApi()
 	api.set_position(40.7127837, -74.005941, 0.0)
-	api.login('ptc', 'usernameprefix{}'.format(num), 'password')
-	time.sleep(12)
+	api.login('ptc', username, password)
+	time.sleep(2)
 	req = api.create_request()
 	req.mark_tutorial_complete(tutorials_completed = 0, send_marketing_emails = False, send_push_notifications = False)
 	response = req.call()
-	print('Verified usernameprefix{}'.format(num))
+	print('Accepted Terms of Service for {}'.format(username))
 	#print('Response dictionary: \r\n{}'.format(pprint.PrettyPrinter(indent=4).pformat(response)))
 
-threads = []
-for i in range(50):
-	t = threading.Thread(target=worker, args=(i+1000,))
-	threads.append(t)
-	t.start()
+accept_tos('username', 'password')
